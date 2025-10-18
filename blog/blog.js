9a1173a51540ff07d16e1094d5156e51b8b23330
heathersego87 <heathersego87@yahoo.com>
2025-10-18 14:09:14 -0500

    Blog changes

---
+++ blog/blog.js
@@ -0,0 +1,30 @@
+// blog.js
+// Contains the data for two books that will be used in the blog project
+
+const books = [
+  {
+    title: "Septimus Heap Book One: Magyk",
+    date: "July 5, 2022",
+    age: "10–14",
+    genre: "Fantasy",
+    rating: "★★★★",
+    image: "images/magyk.jpg",
+    alt: "Book cover for Septimus Heap Book One: Magyk",
+    description:
+      "If you enjoy stories about seventh sons of seventh sons and magyk, this is the book for you."
+  },
+  {
+    title: "Magnus Chase Book One: Sword of Summer",
+    date: "December 12, 2021",
+    age: "12–16",
+    genre: "Fantasy",
+    rating: "★★★★★",
+    image: "images/magnus.jpg",
+    alt: "Book cover for Magnus Chase Book One: Sword of Summer",
+    description:
+      "If you like Norse mythology and fast-paced adventures, this one’s a must-read."
+  }
+];
+
+// Log the books to verify the data is loading correctly
+console.log("Book data loaded:", books);
