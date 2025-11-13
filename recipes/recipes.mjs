9f765364700a36995815370e0c9c9927e441f288
heathersego87 <heathersego87@yahoo.com>
2025-11-12 20:27:43 -0600

    recipes1

---
+++ recipes/recipes.mjs
@@ -0,0 +1,41 @@
+export const recipes = [
+  {
+    name: "Apple Crisp",
+    image: "apple-crisp.jpg",
+    rating: 5,
+    description:
+      "A warm and comforting dessert made with baked apples and a crispy oat topping. Perfect with vanilla ice cream."
+  },
+
+  {
+    name: "Black Beans and Rice",
+    image: "black-beans-and-rice.jpg",
+    rating: 4,
+    description:
+      "A flavorful and nutritious dish made with black beans, seasoned rice, and aromatic spices."
+  },
+
+  {
+    name: "Chicken Curry",
+    image: "chicken-curry.jpg",
+    rating: 5,
+    description:
+      "Tender chicken simmered in a rich, flavorful curry sauce served with rice or naan bread."
+  },
+
+  {
+    name: "Chocolate Chip Cookies",
+    image: "chocolate-chip-cookies.jpg",
+    rating: 5,
+    description:
+      "Soft, chewy cookies filled with rich chocolate chips. A classic, irresistible treat."
+  },
+
+  {
+    name: "Sweet Potato Waffle",
+    image: "sweet-potato-waffle-md.jpg",
+    rating: 4,
+    description:
+      "A delicious sweet potato waffle that is crispy on the outside, fluffy inside, and perfect for breakfast."
+  }
+];
